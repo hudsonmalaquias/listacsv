5ca04d0739b06511fb61a3e776d7ee34c21844f7
hudsonmalaquias <89480045+hudsonmalaquias@users.noreply.github.com>
2021-09-20 11:14:47 -0300

    Add files via upload

--- new 2.py
+++ new 2.py
@@ -11,50 +11,61 @@ for row in rows:
     split_row = row.split(',')
     full_data.append(split_row)
 
-#criar uma variável para receber os dados excluindo a possibilidade de iniciar a linha com aspas duplas
-lista1 = []
-for i in full_data[0]:
-    lista1.append(i.strip('""'))
+#cria a variável count para contar quantas linhas existem no arquivo
+count = 0
+nada = ['']
+for row in full_data:
+    if row != nada:
+        count += 1
         
+#cria uma variável para indicar a posição da linha que o script irá rodar
+aluno = 0
 
-#cirar uma variável para receber apenas as letras das respostas
-lista2 = []
-for i in lista1:
-    if i[:2] == "a)" or i[:2] == "b)" or i[:2] == "c)" or i[:2] == "d)" or i[:2] == "e)" :
-        lista2.append(i[:1])
+#cria um loop while enquanto o número de alunos for menor que a quantidade de linhas, irá executar o script
+while aluno < count:
 
-#abrir o arquivo com o gabarito na variável "g"
-g = open("Pasta3.csv", "r", encoding = "utf8")
+    #criar uma variável para receber os dados excluindo a possibilidade de iniciar a linha com aspas duplas
+    lista1 = []
+    for i in full_data[aluno]:
+        lista1.append(i.strip('""'))
+        
+        
+    #cirar uma variável para receber apenas as letras das respostas
+    lista2 = []
+    for i in lista1:
+        if i[:2] == "a)" or i[:2] == "b)" or i[:2] == "c)" or i[:2] == "d)" or i[:2] == "e)" :
+            lista2.append(i[:1])
 
-#criar uma variável "datas" para receber a leitura dos dados
-datas = g.read()
+    #abrir o arquivo com o gabarito na variável "g"
+    g = open("Pasta3.csv", "r", encoding = "utf8")
 
-#criar uma lista para receber os dados splitados por vírgula
-linhas = datas.split(',')
+    #criar uma variável "datas" para receber a leitura dos dados
+    datas = g.read()
 
-#criar uma lista para receber os dados do gabarito
-respostas = []
-for i in linhas:
-    respostas.append(i)
+    #criar uma lista para receber os dados splitados por vírgula
+    linhas = datas.split(',')
 
-#criar variáveis para percorrer os itens das listas de respostas e gabarito
-lst = 0
-res = 0
+    #criar uma lista para receber os dados do gabarito
+    respostas = []
+    for i in linhas:
+        respostas.append(i)
 
-#criar uma lista para receber os dados com a pontuação fazendo a comparação entre os itens da lista de respostas com a lista do gabarito
-teste = []
-for i in lista2:
-    if lista2[lst] == respostas[res]:
-        teste.append("certo")
-    else:
-        teste.append("errado")
-    lst += 1
-    res += 1
+    lst = 0
+    res = 0
+    teste = []
 
-#variável para contar quantos acertos 
-x = teste.count("certo")
+    for i in lista2:
+        if lista2[lst] == respostas[res]:
+            teste.append("certo")
+        else:
+            teste.append("errado")
+        lst += 1
+        res += 1
 
-#cálculo para obter a nota
-y = x * 0.106
+    x = teste.count("certo")
 
-print(y)
+    y = round(x * 0.106,3)
+
+    print("ALUNO: %s \nNOTA: %s" % (lista1[3], y))
+    
+    aluno += 1
